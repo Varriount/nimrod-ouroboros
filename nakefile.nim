12f20a4dd0dd8b0fee2332a7572b6579fc2b5a4a
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-08-24 22:44:52 +0200

    Adds custom local append data test to nakefile.

--- nakefile.nim
+++ nakefile.nim
@@ -25,3 +25,12 @@ task "docs", "Generates export API docs for for the modules":
 task "local_install", "Copies " & alchemy_exe & " to " & alchemy_dest:
   if shell("nimrod c", alchemy_exe):
     copyFileWithPermissions(alchemy_exe, alchemy_dest)
+
+task "gradha_test", "Random tests, likely useless unlee you are gradha":
+  direShell("nimrod c", alchemy_exe)
+  let
+    nimrod_exe = findExe("nimrod")
+    test_exe = "tests" / "1" / "nimrod"
+  copyFileWithPermissions(nimrod_exe, test_exe)
+  direShell("./alchemy", "-o", test_exe, "-v", "../root/lib")
+  direShell("./alchemy", "-l", test_exe)
